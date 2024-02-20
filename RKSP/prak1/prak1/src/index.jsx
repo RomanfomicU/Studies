93551035b48c19d408ab414233aae4f3aab6edd0
unknown <romanfomicm@gmail.com>
2024-02-20 11:09:46 +0300

    RKSP_prak1_2

---
+++ RKSP/prak1/prak1/src/index.jsx
@@ -0,0 +1,17 @@
+import React, { StrictMode } from "react";
+import * as ReactDOMClient from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+// import "./css/style.css";
+
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
+
+app.render(
+  <StrictMode>
+    <Header header="Автосалон Black Lightning" />
+    <Body
+      title="В продаже новые автомобили"
+      text="Новые немецкие автомобили поступили в продажу в нашем автосалоне. Гарантия 20 лет."
+    />
+  </StrictMode>
+);
