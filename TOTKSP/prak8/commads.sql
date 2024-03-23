72a8fc4777294b5bc412352e7abddb216678f40c
Roman Fomichev <romanfomic@gmail.com>
2024-03-23 19:10:15 +0300

    TOTKSP_prak8_fix

--- TOTKSP/prak8/commads.sql
+++ TOTKSP/prak8/commads.sql
@@ -63,10 +63,23 @@ UPDATE example_table SET value = 'Updated 1 by Session 3' WHERE id = 1;
 -- Task 3 --
 -- Session 1 --
 
-BEGIN;
-UPDATE example_table SET value = 'Updated by Session 1' WHERE id = 1;
+psql -U fomic -d fomic_db -h localhost -p 5432
+
+psql -U fomic -d fomic_db -h localhost -p 5432 -c "drop table test"
+psql -U fomic -d fomic_db -h localhost -p 5432 -c "create table test(id integer primary key generated always as identity, n float)"
+psql -U fomic -d fomic_db -h localhost -p 5432 -c "insert into test(n) select random() from generate_series(1,1000000)"
+
+psql -U fomic -d fomic_db -h localhost -p 5432 << EOF
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+UPDATE test SET n = (select id from test order by id asc limit 1 for update);
+COMMIT;
+EOF
+
 
 -- Session 2 --
 
-BEGIN;
-UPDATE example_table SET value = 'Updated by Session 2' WHERE id = 1;
+psql -U fomic -d fomic_db -h localhost -p 5432 << EOF
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+UPDATE test SET n = (select id from test order by id desc limit 1 for update);
+COMMIT;
+EOF
