e7fc59a824ab40e8fbd0ecc0edb4e7d2ab0f1ffa
Roman Fomichev <romanfomic@gmail.com>
2024-11-13 20:22:07 +0300

    RKSP prak 7

---
+++ 7_sem/RKSP/prak_7/src/main/java/com/fomic/dragonsrksp7/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.fomic.dragonsrksp7.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ 7_sem/RKSP/prak_7/src/test/java/com/verarchik/dragonsrksp7/DragonsRksp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomic.dragonsrksp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonsRksp7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
